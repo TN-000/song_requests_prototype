03b05b92bd686c96a8b6e3e4ec5bf16340355a51
TN000 <69354222+TN-000@users.noreply.github.com>
2022-03-29 18:07:15 +0100

    Add files via upload

---
+++ SpotifyWebAPI-main/app.js
@@ -0,0 +1,230 @@
+var redirect_uri = "http://127.0.0.1:5500/SpotifyWebAPI-main/"; //change to whatever URI you want
+// (make sure to make this change in the Spotify Developer Dashboard!)
+ 
+
+var client_id = ""; 
+var client_secret = ""; // In a real app you should not expose your client_secret to the user
+
+var access_token = null;
+var refresh_token = null;
+var currentPlaylist = "";
+var radioButtons = [];
+
+const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const TOKEN = "https://accounts.spotify.com/api/token";
+const PLAYLISTS = "https://api.spotify.com/v1/me/playlists";
+const DEVICES = "https://api.spotify.com/v1/me/player/devices";
+const PLAY = "https://api.spotify.com/v1/me/player/play";
+const PAUSE = "https://api.spotify.com/v1/me/player/pause";
+const NEXT = "https://api.spotify.com/v1/me/player/next";
+const PREVIOUS = "https://api.spotify.com/v1/me/player/previous";
+const PLAYER = "https://api.spotify.com/v1/me/player";
+const TRACKS = "https://api.spotify.com/v1/playlists/{{PlaylistId}}/tracks";
+const CURRENTLYPLAYING = "https://api.spotify.com/v1/me/player/currently-playing";
+const SHUFFLE = "https://api.spotify.com/v1/me/player/shuffle";
+
+function onPageLoad(){
+    client_id = localStorage.getItem("client_id");
+    client_secret = localStorage.getItem("client_secret");
+    if ( window.location.search.length > 0 ){
+        handleRedirect();
+    }
+    else{
+        access_token = localStorage.getItem("access_token");
+        if ( access_token == null ){
+            // don't have an access token so present token section
+            document.getElementById("tokenSection").style.display = 'block';  
+        }
+        else {
+            // have an access token so present song request section
+            document.getElementById("songRequestSection").style.display = 'block';  
+            refreshPlaylists();
+        }
+    }
+}
+
+function handleRedirect(){
+    let code = getCode();
+    fetchAccessToken( code );
+    window.history.pushState("", "", redirect_uri); // remove param from url
+}
+
+function getCode(){
+    let code = null;
+    const queryString = window.location.search;
+    if ( queryString.length > 0 ){
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code')
+    }
+    return code;
+}
+
+function requestAuthorisation(){
+    client_id = document.getElementById("clientId").value;
+    client_secret = document.getElementById("clientSecret").value;
+    localStorage.setItem("client_id", client_id);
+    localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
+
+    let url = AUTHORIZE;
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private playlist-modify-public playlist-modify-private";
+    window.location.href = url; // Show Spotify's authorization screen
+}
+
+function fetchAccessToken( code ){
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code; 
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+}
+
+function refreshAccessToken(){
+    refresh_token = localStorage.getItem("refresh_token");
+    let body = "grant_type=refresh_token";
+    body += "&refresh_token=" + refresh_token;
+    body += "&client_id=" + client_id;
+    callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body){
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        if ( data.access_token != undefined ){
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if ( data.refresh_token  != undefined ){
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+function callApi(method, url, body, callback){
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+    xhr.send(body);
+    xhr.onload = callback;
+}
+
+function refreshPlaylists(){
+    callApi( "GET", PLAYLISTS, null, handlePlaylistsResponse );
+}
+
+function searchTrack(){
+    var song = document.forms["SongRequest"]["song"].value;
+    song = song + " " + document.forms["SongRequest"]["artist"].value
+    var i = 0, strLength = song.length;
+    for(i; i < strLength; i++) {
+        song = song.replace(" ", "+");
+    }
+    var uri = "https://api.spotify.com/v1/search?query=" + song + "&type=track&locale=en-US%2Cen%3Bq%3D0.9&offset=0&limit=1";
+    callApi( "GET", uri, null, handleSearchTrackResponse );
+}
+
+function handleSearchTrackResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        addTrackToPlaylist(data.tracks.items[0].id);
+    }
+    else if ( this.status == 401 ){
+        refreshAccessToken()
+    }
+    else {
+        console.log(this.responseText);
+        alert("Enter a song name");
+    }  
+}
+
+function handlePlaylistsResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        removeAllItems( "eventList" );
+        data.items.forEach(item => addPlaylist(item));
+        document.getElementById('eventList').value=currentPlaylist;
+    }
+    else if ( this.status == 401 ){
+        refreshAccessToken()
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+function addPlaylist(item){
+    let node = document.createElement("option");
+    node.value = item.id;
+    node.innerHTML = item.name;
+    document.getElementById("eventList").appendChild(node); 
+}
+
+function removeAllItems( elementId ){
+    let node = document.getElementById(elementId);
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+}
+
+function handleApiResponse(){
+    if ( this.status == 200){
+        console.log(this.responseText);
+        setTimeout(currentlyPlaying, 2000);
+    }
+    else if ( this.status == 204 ){
+        setTimeout(currentlyPlaying, 2000);
+    }
+    else if ( this.status == 401 ){
+        refreshAccessToken()
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }    
+}
+
+function addTrackToPlaylist(song_id){
+    let playlist_id = document.getElementById("eventList").value;
+    let song_search = "spotify:track:" + song_id
+    song_search = encodeURI(song_search)
+    if ( playlist_id.length > 0 ){
+        url = TRACKS.replace("{{PlaylistId}}", playlist_id);
+        callApi( "POST", url+"?uris="+song_search , null, handleAddTrackToPlaylistResponse );
+    }
+}
+
+function handleAddTrackToPlaylistResponse(){
+    if ( this.status == 401 ){
+        refreshAccessToken()
+    }
+    else {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        document.getElementById("resultText").innerHTML = "Hooray! You added the song '" + document.forms["SongRequest"]["song"].value + "' by " + document.forms["SongRequest"]["artist"].value + " to the '" + document.getElementById("eventList").options[document.getElementById("eventList").selectedIndex].text + "' playlist!";
+    }
+}
